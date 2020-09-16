b2377e60fe9afdc8a50406b9b268bad4051aafe7
Paul Wamaria <paulwamaria@gmail.com>
2020-09-16 16:36:03 +0300

    Add Profile Api

--- accounts/authApi.py
+++ accounts/authApi.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import UserSerializer, RegistrationSerializer, LoginSerializer
+from .serializers import UserSerializer, RegistrationSerializer, LoginSerializer, ProfileSerializer
 from knox.views import LoginView
 from rest_framework.authentication import BasicAuthentication
 
@@ -53,4 +53,17 @@ class UserAPI(generics.GenericAPIView):
     serializer_class=UserSerializer
     def get_object(self):
         return self.request.user
+
+
+# ProfileAPI
+class ProfileAPI(generics.GenericAPIView):
+    '''
+    This class will return user details when the user endpoint is called
+    '''
+    permission_classes=[
+        permissions.IsAuthenticated,
+    ]
+    serializer_class=ProfileSerializer
+    def get_object(self):
+        return self.request.profile
 
