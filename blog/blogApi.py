af9bb97aa19ae5ecb0b3f6837bb7033601c1a95a
Paul Wamaria <paulwamaria@gmail.com>
2020-09-30 21:07:57 +0300

    import the decorator, api_view

--- blog/blogApi.py
+++ blog/blogApi.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .serializers import CropSerializer, ArticleSerializer
+from rest_framework.decorators import action, api_view
 
 # CropAPI
 class CropAPI(generics.GenericAPIView):
